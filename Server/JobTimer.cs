8c0b4cb2944b3569e8439f4a956b96dd2191cf4c
Rluan <jeong22437747@gmail.com>
2021-11-19 23:10:58 +0900

    del serverCOre

--- Server/JobTimer.cs
+++ Server/JobTimer.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ServerCore;
 namespace Server
 {
     struct JobTimerElem : IComparable<JobTimerElem>
